6fa4686f49b22437c0ef303ecf3a3409e1e3649e
Avinash Guleria <avinashguleria1009@gmail.com>
2025-02-03 22:29:57 +0530

    change

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = 8008;
+
+app.get("/", (req, res) => {
+    res.send("Hello There My name is Avinash This is one of my first project to publish")
+});
+
+app.listen(8008, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
